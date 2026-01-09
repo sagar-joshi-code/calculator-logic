e882d4da73c7d203464c009ed2deb2e45e7debbe
sagar joshi <sagarjo637@gmail.com>
2026-01-09 18:11:32 +0545

    practiced calculator logic

---
+++ script.js
@@ -0,0 +1,17 @@
+let num1 = Number(prompt("Enter first number"));
+let operator = prompt("Enter operator(+ , - ,* , /)");
+let num2 = Number(prompt("Enter second number"));
+
+let result;
+if (operator === "+") {
+  result = num1 + num2;
+} else if (operator === "-") {
+  result = num1 - num2;
+} else if (operator === "*") {
+  result = num1 * num2;
+} else if (operator === "/") {
+  result = num1 / num2;
+} else {
+  result = "invalid operator";
+}
+console.log(result);
